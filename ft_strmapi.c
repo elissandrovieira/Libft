4176459a2872e60a90a827395841fef30da35ac2
Elissandro Vieira <teofiloeli@gmail.com>
2024-10-08 16:15:07 -0300

    fixing ft_strmapi function

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -17,12 +17,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*str;
 	unsigned int	i;
 
-	str = (char *)malloc(sizeof(char) * ft_strlen(s));
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
 	i = 0;
 	while (s[i] != 0)
 	{
 		str[i] = f(i, s[i]);
 		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
