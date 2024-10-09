241e763c15f0b8d6617601f865c36f530c43d10d
Elissandro Vieira <teofiloeli@gmail.com>
2024-10-09 10:44:10 -0300

    fixing ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: eteofilo <eteofilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 20:24:58 by eteofilo          #+#    #+#             */
-/*   Updated: 2024/10/08 21:57:03 by eteofilo         ###   ########.fr       */
+/*   Updated: 2024/10/09 10:43:55 by eteofilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list	*node;
 	t_list	*to_free;
 
+	node = *lst;
 	if (!lst || !del)
 		return ;
-	lst = NULL;
-	while (node->next != 0)
+	while (node)
 	{
+		to_free = node->next;
 		del(node->content);
-		to_free = node;
-		node = node->next;
-		free(to_free);
+		free(node);
+		node = to_free;
 	}
 }
